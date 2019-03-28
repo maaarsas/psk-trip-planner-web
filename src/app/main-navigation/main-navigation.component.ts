faa9b7b22a0d509573d66ecddb8ef535cb70b5ac
Martynas Šapalas <martynas.sapalas@gmail.com>
2019-03-28 13:49:40 +0200

    Make property isNavbarCollapsed public in MainNavigationComponent

--- src/app/main-navigation/main-navigation.component.ts
+++ src/app/main-navigation/main-navigation.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 })
 export class MainNavigationComponent {
 
-  private isNavbarCollapsed = true;
+  isNavbarCollapsed = true;
 
   constructor(
     private authService: AuthenticationService,
