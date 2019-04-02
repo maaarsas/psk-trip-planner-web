6f1b22735d7071570d8e0008cffe940188190195
Herkus Simenas <neherkus@gmail.com>
2019-04-02 10:19:19 +0300

    Base trip form on template

--- src/app/trip-form/trip-form.component.ts
+++ src/app/trip-form/trip-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-trip-form',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripFormComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+  }
 }
