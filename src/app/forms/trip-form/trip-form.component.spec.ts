370732e35bdb95346b6f59c564f74dc8657e7e08
Herkus Simenas <neherkus@gmail.com>
2019-04-02 21:13:47 +0300

    Fix button disable

--- src/app/forms/trip-form/trip-form.component.spec.ts
+++ src/app/forms/trip-form/trip-form.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TripFormComponent } from './trip-form.component';
+import {TripFormComponent} from './trip-form.component';
 
 describe('TripFormComponent', () => {
   let component: TripFormComponent;
@@ -8,9 +8,9 @@ describe('TripFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TripFormComponent ]
+      declarations: [TripFormComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/forms/trip-form/trip-form.component.ts
+++ src/app/forms/trip-form/trip-form.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-trip-form',
@@ -7,10 +8,19 @@ import {Component, OnInit} from '@angular/core';
 })
 export class TripFormComponent implements OnInit {
 
-  constructor() {
+  tripForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
   }
 
   ngOnInit() {
+    this.createTripForm();
+  }
+
+  createTripForm() {
+    this.tripForm = this.formBuilder.group({});
   }
 
   onSubmit() {
