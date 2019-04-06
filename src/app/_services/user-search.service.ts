c73f5d6ad948451a5146ba8680ff3f0b18e1b5d0
Herkus Simenas <neherkus@gmail.com>
2019-04-06 16:35:10 +0300

    Don't display persons if input is empty

--- src/app/_services/user-search.service.ts
+++ src/app/_services/user-search.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {Person} from '../_models/trip';
 
