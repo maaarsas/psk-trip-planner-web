80ac69198c3f6f21071a4f470cc97af8f6fd1d41
MigleBudryte <budryte.migle@gmail.com>
2019-04-06 14:52:44 +0300

    Fix default start date constant

--- src/app/_constants/trip-list.const.ts
+++ src/app/_constants/trip-list.const.ts
@@ -2,6 +2,6 @@ export const PAGE_SIZE_OPTIONS = [10, 20, 50];
 export const DEFAULT_PAGE_SIZE = 10;
 export const DEFAULT_PAGE = 1;
 
-let today = new Date();
-export const DEFAULT_START_DATE_FROM_MODEL = {year: today.getFullYear(), month: today.getMonth()+1, day: today.getDay()}
-export const DEFAULT_START_DATE_FROM = new Date(today.getTime() - (today.getTimezoneOffset() * 60000 )).toISOString().split("T")[0];
+const today = new Date();
+export const DEFAULT_START_DATE_FROM_MODEL = {year: today.getFullYear(), month: today.getMonth()+1, day: today.getDate()};
+export const DEFAULT_START_DATE_FROM = today.getFullYear() + '-' + ('0'+ (today.getMonth()+1)).slice(-2) + '-' + ('0'+ today.getDate()).slice(-2);
