47d239ec8755b96b58c8b3b2808818a8550413da
Herkus Simenas <neherkus@gmail.com>
2019-04-07 18:45:20 +0300

    FIX#2: Use typeahead

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -1,9 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {UserSearchService} from '../_services/user-search.service';
 import {Person} from '../_models/trip';
-import {FormControl} from '@angular/forms';
 import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
-import {of} from 'rxjs';
+import {Observable, of} from 'rxjs';
 
 @Component({
   selector: 'app-user-search',
@@ -14,25 +13,26 @@ import {of} from 'rxjs';
 export class UserSearchComponent implements OnInit {
 
   results: Person[];
-  queryField: FormControl = new FormControl();
 
-  constructor(private searchService: UserSearchService) {
-  }
-
-  ngOnInit() {
-    this.queryField.valueChanges.pipe(debounceTime(400),
-      distinctUntilChanged(), switchMap((query) => {
-          if (query.trim() !== '') {
+  search = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((query) => {
+          if (query.trim() !== '' && query.trim().length >= 2) {
             return this.searchService.search(query.trim());
           } else {
             return of([]);
           }
         }
-      ))
-      .subscribe(response => {
-          this.results = response;
-        }
-      );
+      )
+    ).subscribe(response => {
+      this.results = response;
+    })
+
+  constructor(private searchService: UserSearchService) {
   }
 
+  ngOnInit() {
+  }
 }
