e22fa2dc3751d6d138c636a06c6a24034aa52cb0
Martynas Šapalas <martynas.sapalas@gmail.com>
2019-05-11 16:41:36 +0300

    Start implementing trip merge modal

--- src/app/planning/trip-merge-modal/trip-merge-modal.component.ts
+++ src/app/planning/trip-merge-modal/trip-merge-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Trip } from '../../_models/trip';
+import { TripService } from '../../_services/trip.service';
 
 @Component({
   selector: 'app-trip-merge-modal-component',
@@ -9,10 +10,29 @@ import { Trip } from '../../_models/trip';
 })
 export class TripMergeModalComponent implements OnInit {
 
-  @Input() toTrip: Trip;
+  private _toTrip: Trip;
 
-  constructor(public activeModal: NgbActiveModal) {}
+  private mergeableTrips: Trip[];
+
+  @Input() set toTrip(value: Trip) {
+    this._toTrip = value;
+    this.loadMergeableTrips();
+  }
+
+  get toTrip(): Trip {
+    return this._toTrip;
+  }
+
+  constructor(public activeModal: NgbActiveModal, private tripService: TripService) {}
 
   ngOnInit() {}
+
+  private loadMergeableTrips() {
+    this.tripService.getMergeableTrips(this.toTrip).subscribe(trips => this.mergeableTrips = trips);
+  }
+
+  public getShortTripSummary(trip: Trip) {
+    return trip.startDate + ' - ' + trip.endDate + ' | ' + trip.fromOffice.title + ' - ' + trip.toOffice.title;
+  }
 
 }
