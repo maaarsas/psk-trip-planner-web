5de0b6697ca47fccc3a9b13b57deedaeefd581ae
Martynas Šapalas <martynas.sapalas@gmail.com>
2019-05-11 14:34:50 +0300

    Show error when user tries to access unauthorized functions

--- src/app/_guards/role.guard.ts
+++ src/app/_guards/role.guard.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivateChild } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
 import { UserService } from '../_services/user.service';
+import { ErrorModalComponent } from '../error-modal/error-modal.component';
 
 @Injectable({ providedIn: 'root' })
 export class RoleGuard implements CanActivateChild {
 
-    constructor(private router: Router, private userService: UserService) { }
+    constructor(
+      private router: Router,
+      private userService: UserService,
+      private modalService: NgbModal,
+      private translate: TranslateService,
+    ) { }
 
     canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
       // only block access when there are allowed roles specified for the route and current user doesn't have any of them
@@ -18,6 +26,9 @@ export class RoleGuard implements CanActivateChild {
         element => allowedRoles.includes(element)
       );
       if (matchingAllowedRoles.length === 0) {
+        const modalRef = this.modalService.open(ErrorModalComponent);
+        this.translate.get('no-access-rights-error')
+          .subscribe(translation =>  modalRef.componentInstance.errorMessage = translation);
         return false;
       }
       return true;
