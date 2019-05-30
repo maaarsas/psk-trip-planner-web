7cca69883fc3d92dee610dfdc15a1498bfcaa3e4
Herkus Simenas <neherkus@gmail.com>
2019-05-30 06:25:22 +0300

    Fix adding people

--- src/app/forms/trip-form/trip-form.component.ts
+++ src/app/forms/trip-form/trip-form.component.ts
@@ -1,14 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {OfficeService} from '../../_services/office.service';
-import {Office, Person} from '../../_models/trip';
-import {Observable, of} from 'rxjs';
-import {debounceTime, delay, distinctUntilChanged, map, switchMap} from 'rxjs/operators';
+import {Person, TripParticipation} from '../../_models/trip';
+import {Office} from '../../_models/office';
+import {Observable} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map, switchMap} from 'rxjs/operators';
 import {UserSearchService} from '../../_services/user-search.service';
-import {resolve} from 'q';
-import {async} from 'rxjs/internal/scheduler/async';
-
-const names = ['Herkus', 'Greta', 'Povilas'];
+import {UserService} from '../../_services/user.service';
+import {User} from '../../_models/user';
 
 @Component({
   selector: 'app-trip-form',
@@ -19,17 +18,25 @@ export class TripFormComponent implements OnInit {
 
   tripForm: FormGroup;
 
+  public model: Person;
+
   offices: Office[];
   toOffice: string;
   fromOffice: string;
-  persons: Person[];
-  personsNames: Observable<string[]>;
-  alsoPersonsNames: string[];
+  persons: Person[] = new Array();
+  columns: string[];
+  tripParticipations: TripParticipation[];
+  isPlane: boolean;
+  isCar: boolean;
+  isAccommodation: boolean;
+  organizer: Person;
+  user: User;
 
   constructor(
     private formBuilder: FormBuilder,
     private officeService: OfficeService,
-    private searchService: UserSearchService
+    private searchService: UserSearchService,
+    private userService: UserService
   ) {
   }
 
@@ -39,41 +46,15 @@ export class TripFormComponent implements OnInit {
       debounceTime(200),
       distinctUntilChanged(),
       switchMap(term => {
-        if (term.trim() !== '' && term.trim().length >= 2) {
-          this.personsNames = this.searchService.search(term.trim()).pipe(map((persons) => {
-            const personsNames = [];
-            for (const person of persons) {
-              personsNames.push(person.name);
-            }
-            return personsNames;
-          }));
-          this.personsNames.subscribe(thing => console.log(thing));
-          return this.personsNames.pipe();
-          /*if (term.trim() !== '' && term.trim().length >= 2) {
-            console.log('pradzia');
-            console.log(this.personsNames);
-            var o = this.getNames(term);
-            this.persons = o;
-            for (const person of this.persons) {
-              this.personsNames.push(person.name);
-            }
-            return o.toPromise().then(response => {
-              console.log('response');
-              console.log(response);
-              this.persons = response;
-              for (const person of this.persons) {
-                this.personsNames.push(person.name);
+          if (term.trim() !== '' && term.trim().length >= 2) {
+            const persons = this.searchService.search(term.trim()).pipe(map((foundPersons) => {
+              const personsNames = [];
+              for (const person of foundPersons) {
+                personsNames.push(person);
               }
-              console.log('then');
-              console.log(this.personsNames);
-              // return this.personsNames.slice(0, 10);
-              // return names;
-            });
-
-            // console.log('po subscribe');
-            // console.log(result);
-            // return this.personsNames;
-            // return names;*/
+              return personsNames;
+            }));
+            return persons;
           } else {
             return [];
           }
@@ -81,16 +62,16 @@ export class TripFormComponent implements OnInit {
       )
     )
 
-  /*async getNames(term: string) {
-    var o = await this.searchService.search(term.trim());
-    this.persons = o;
-  }*/
-
   ngOnInit() {
     this.createTripForm();
     this.officeService.getOffices().subscribe(response => {
       this.offices = response;
     });
+    this.columns = ['Vardas', ''];
+    this.isAccommodation = false;
+    this.isCar = false;
+    this.isPlane = false;
+    this.user = this.userService.getCurrentUser();
   }
 
   createTripForm() {
@@ -107,4 +88,38 @@ export class TripFormComponent implements OnInit {
   saveToSelection(selection: any) {
     this.toOffice = selection;
   }
+
+  addPerson(item: Person) {
+    let isInList = false;
+    console.log('here');
+    console.log(this.persons);
+    for (const person of this.persons) {
+      console.log('here');
+      if (person.id === item.id) {
+        console.log('already in the list');
+        isInList = true;
+      }
+    }
+    if (!isInList) {
+      this.persons.push(item);
+    }
+    console.log(this.persons);
+  }
+
+  deletePerson(id: number) {
+    for (const person of this.persons) {
+      if (person.id === id) {
+        this.persons = this.persons.filter(item => item !== person);
+      }
+    }
+  }
+
+  submit() {
+    this.user = this.userService.getCurrentUser();
+    this.organizer.id = this.user.id;
+    this.organizer.name = this.user.firstName;
+    this.organizer.surname = this.user.lastName;
+  }
+
+  format = (x: {name: string}) => x.name;
 }
